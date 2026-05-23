71cb853a3bc7916663a6d2b9e507a398820cb04c
L9Ojik <synowiec35@gmail.com>
2026-05-23 10:45:56 +0200

    Resolving little pr mess

---
+++ app/DTOs/CurrentUserDTO.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DTOs;
+
+class CurrentUserDTO
+{
+    protected int $Id;
+    protected string $Role;
+    protected bool $Param;
+    protected string $Acc_State;
+    public function __construct(int $Id,string $Role,bool $Param,string $Acc_State)
+    {
+        $this->Id = $Id;
+        $this->Role = $Role;
+        $this->Param = $Param;
+        $this->Acc_State = $Acc_State;
+    }
+    public function getId():int{
+        return $this->Id;
+    }
+    public function setId(int $Id){
+        $this->Id = $Id;
+    }
+    
+    public function getRole():string{
+        return $this->Role;
+    }
+    public function setRole(string $Role){
+        $this->Role = $Role;
+    }
+
+    public function getParam():bool{
+        return $this->Param;
+    }
+    public function setParam(bool $Param){
+        $this->Param = $Param;
+    }
+
+    public function getAcc_State():string{
+        return $this->Acc_State;
+    }
+    public function setAcc_State(string $Acc_State){
+        $this->Acc_State = $Acc_State;
+    }
+}
---
+++ app/DTOs/WProfileDTO.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\DTOs;
+
+class WProfileDTO extends ProfileDTO
+{
+    protected bool $Is_Admin;
+    public function __construct(
+        string $Name,string $Last_Name,
+        string $Login,string $Creation_Date,
+        array $History,bool $Is_Admin
+        )
+    {
+        $this->Name = $Name;
+        $this->Last_Name = $Last_Name;
+        $this->Login = $Login;
+        $this->Creation_Date = $Creation_Date;
+        $this->History = $History;
+        $this->Is_Admin = $Is_Admin;
+    }
+
+    public function getIs_Admin():bool{
+        return $this->Is_Admin;
+    }
+    public function setIs_Admin(bool $Is_Admin){
+        $this->Is_Admin = $Is_Admin;
+    }
+}
